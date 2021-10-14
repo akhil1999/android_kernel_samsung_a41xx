8dde462a9618aed9851e167f96934c940b682829
akhil1999 <akhileshbhuvanendra@gmail.com>
2021-10-14 23:03:25 +0530

    Add toolchain paths

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r353983c/bin/clang
+rm -rf out/
+export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CC=$(pwd)/toolchain/clang/proton-clang-master/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=r
 
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
-
+make clean && make mrproper
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a41_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j10
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
